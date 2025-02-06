648dc3085a09cc22d16841f2aea2ec6f1c9443ae
JulianUbu <thelastpocket@gmail.com>
2025-02-06 15:35:04 -0800

    Got pointcloud to be visible on rviz

--- sense_pub.py
+++ sense_pub.py
@@ -8,6 +8,8 @@ import sensor_msgs_py.point_cloud2 as pc2
 from std_msgs.msg import Header
 import numpy as np
 import cv2
+from geometry_msgs.msg import TransformStamped
+import tf2_ros
 from cv_bridge import CvBridge
 import pyrealsense2 as rs
 
@@ -22,8 +24,8 @@ class JPointPublisher(Node):
         # Start the RealSense pipeline
         self.pipe = rs.pipeline()
         config = rs.config()
-        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 15)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)
         self.pipe.start(config)
         
         
@@ -32,7 +34,9 @@ class JPointPublisher(Node):
         self.align = rs.align(rs.stream.color)
 
         
-        self.timer = self.create_timer(0.1, self.publish_pointcloud)
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self, )
+
+        self.timer = self.create_timer(1, self.publish_pointcloud)
 
     def publish_pointcloud(self):
         
@@ -48,7 +52,7 @@ class JPointPublisher(Node):
         vertices = np.asanyarray(points.get_vertices())
 
         header = Header()
-        #header.stamp = datetime.datetime.now()
+        header.stamp = self.get_clock().now().to_msg()
         header.frame_id = "camera_link"
 
         cloud_points = []
@@ -58,51 +62,30 @@ class JPointPublisher(Node):
         pointcloud_msg = pc2.create_cloud_xyz32(header, cloud_points)
 
 
-        # # Convert color frame to OpenCV format
-        # color_image = np.asanyarray(color_frame.get_data())
-        # image_msg = self.bridge.cv2_to_imgmsg(color_image, encoding="bgr8")
-        
-        # # Create PointCloud2 message
-        # pointcloud_msg = PointCloud2()
-        # pointcloud_msg.header.stamp = self.get_clock().now().to_msg()
-        # pointcloud_msg.header.frame_id = "camera_link"
-        
-        # # Allocate data for the point cloud
-        # width = depth_frame.get_width()
-        # height = depth_frame.get_height()
-        
-        # # Set PointCloud2 fields (x, y, z, intensity)
-        # pointcloud_msg.fields = [
-        #     PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1),
-        #     PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1),
-        #     PointField(name="z", offset=8, datatype=PointField.FLOAT32, count=1)
-        # ]
-
-        # #points = np.zeros((height, width, 4), dtype=np.float32)  # 4 columns: x, y, z, intensity
-        
-        # #pointcloud_msg.row_step = pointcloud_msg.width * 16  # 4 bytes per float for x, y, z, intensity
-        # #pointcloud_msg.data = bytearray(pointcloud_msg.row_step * pointcloud_msg.height)
-        
-        # # Fill the point cloud data
-        # #ptr = np.array(pointcloud_msg.data)
-        # for y in range(height):
-        #     for x in range(width):
-        #         depth_value = depth_frame.get_distance(x, y)
-
-        #         points[y, x, 0] = (x - width / 2) * depth_value  # x
-        #         points[y, x, 1] = (y - height / 2) * depth_value  # y
-        #         points[y, x, 2] = depth_value  # z
-        #         points[y, x, 3] = depth_value     
-        
-        # pointcloud_msg.data = points.flatten().tobytes()
-        # pointcloud_msg.width = width
-        # pointcloud_msg.height = height
-        # pointcloud_msg.row_step = width * 16
-
-
         # Publish the point cloud and the image
         self.pointcloud_pub.publish(pointcloud_msg)
         #self.color_publisher.publish(image_msg)
+
+        self.broadcast_transform()
+        
+    def broadcast_transform(self):
+        # Example transform broadcasting from "world" to "camera_link"
+        transform = TransformStamped()
+        transform.header.stamp = self.get_clock().now().to_msg()
+        transform.header.frame_id = "world"
+        transform.child_frame_id = "camera_link"
+
+        # Assuming the camera is at the origin, but you can change this if needed
+        transform.transform.translation.x = 0.0
+        transform.transform.translation.y = 0.0
+        transform.transform.translation.z = 0.0
+
+        transform.transform.rotation.x = 0.0
+        transform.transform.rotation.y = 0.0
+        transform.transform.rotation.z = 0.0
+        transform.transform.rotation.w = 1.0
+
+        self.tf_broadcaster.sendTransform(transform)
 
 def main(args=None):
     print("Starting publish...")
